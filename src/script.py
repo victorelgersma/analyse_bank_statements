a2b1a0ca4a2e13b62578f11ad9c078ffcf5a590f
Victor Elgersma <victor.elgersma41@thg.com>
2024-11-03 11:08:42 +0000

    style: formatting

--- src/script.py
+++ src/script.py
@@ -9,7 +9,10 @@ BLUE = "\033[34m"
 RESET = "\033[0m"
 
 
-# revolut
+print("\n--------------------------------------------------------\n")
+print(f"{BLUE} REVOLUT {RESET}")
+print("\n--------------------------------------------------------\n")
+
 # euro account
 rev_euro_df = pd.read_csv('../data/rev/eur/oct.csv')
 # pound account
@@ -18,29 +21,13 @@ rev_gbp_df = pd.read_csv('../data/rev/gbp/oct.csv')
 # dollar account
 rev_usd_df = pd.read_csv('../data/rev/usd/oct.csv')
 
-# santander current account
-san = pd.read_html(Path('../data/san/current/oct.html'))[0] 
-
-# santander savings account
-
-san = pd.read_html(Path('../data/san/saver/oct.html'))[0] 
-
+# columns
 rev_columns=["Amount", "Description", "Completed Date"]
-san_columns=["Debit/Credit", "Date"]
 
-print("\nRevolut:\n")
-print("\ndata structure")
-print(rev_euro_df.info()) 
-
-# print(rev_df)
-print('info:\n')
-# check column names
 
 print("\nEuro:\n")
 print(rev_euro_df[rev_columns]) 
-
 print("\nGBP:\n")
-
 print(rev_gbp_df[rev_columns]) 
 print("\nUSD:\n")
 print(rev_usd_df[rev_columns]) 
@@ -48,9 +35,17 @@ print(rev_usd_df[rev_columns])
 print("\n--------------------------------------------------------\n")
 print(f"{RED} santander {RESET}")
 print("\n--------------------------------------------------------\n")
-print(san.info())
-print("\nTransactions\n")
 
-print(san)
+# santander current account
+san_current = pd.read_html(Path('../data/san/current/oct.html'))[0] 
 
+# santander savings account
+san_savings = pd.read_html(Path('../data/san/saver/oct.html'))[0] 
+san_columns=["Debit/Credit", "Date"]
+
+print("\nTransactions - Current Account\n")
+print(san_current)
+
+print("\nTransactions - Savings Account\n")
+print(san_savings)
 
