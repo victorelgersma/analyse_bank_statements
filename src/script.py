021b3b9fa1e6fa61e9e8b15a19cf7b9c20211542
Victor Elgersma <victor.elgersma41@thg.com>
2024-11-03 18:17:12 +0000

    feat: added sum totals for all values

--- src/script.py
+++ src/script.py
@@ -3,11 +3,10 @@ from pathlib import Path
 import pandas as pd
 
 MONTH = 'oct'
+BALANCE = 0
 
 # ANSI escape codes for colors
 RED = "\033[31m"
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
 
@@ -33,11 +32,17 @@ rev_columns=["Amount", "Description", "Completed Date"]
 
 
 print("\nEuro Account\n")
+print(rev_euro_df.dtypes)
 print(rev_euro_df[rev_columns]) 
+print("sum", rev_euro_df["Amount"].sum())
+
 print("\nGBP Account\n")
 print(rev_gbp_df[rev_columns]) 
+print("sum", rev_gbp_df["Amount"].sum())
 print("\nUSD Account\n")
 print(rev_usd_df[rev_columns]) 
+
+print("sum", rev_usd_df["Amount"].sum())
 
 print("\n--------------------------------------------------------\n")
 print(f"{RED} SANTANDER - TRANSACTIONS {RESET}")
@@ -54,11 +59,21 @@ def get_encoding(path):
         print(f"Detected encoding: {encoding}")
     return encoding
 
+
 def parse_san(path):
+    # TODO - add doctest with dummy data
+    encoding = get_encoding(path)
     encoding = get_encoding(path)
     df = pd.read_html(Path(path), encoding=encoding, header =3)[0]
     cols = ["Date", "Description", "Money in", "Money Out"]
+    # turn into booleans
+    df['Money in'] = df['Money in'].str.replace('£', '').str.replace(',', '').astype(float)
+    df['Money Out'] = df['Money Out'].str.replace('£', '').str.replace(',', '').astype(float)
+    # Replace NaNs with 0
+    df['Money in'] = df['Money in'].fillna(0)
+    df['Money Out'] = df['Money Out'].fillna(0)
     return df[cols]
+
 # santander current account
 san_current = parse_san('../data/san/current/oct.html')
 # santander savings account
@@ -67,8 +82,15 @@ san_savings = parse_san('../data/san/saver/oct.html')
 san_columns=["Debit/Credit", "Date"]
 
 print("\nCurrent Account\n")
+print(san_current.dtypes)
 print(san_current)
 
+print("total money out", san_current["Money Out"].sum())
+print("total money in", san_current["Money in"].sum())
+print("sum", - san_current["Money Out"].sum() + san_current["Money in"].sum())
 print("\nSavings Account\n")
 print(san_savings)
+print("sum", - san_savings["Money Out"].sum() + san_savings["Money in"].sum())
+
+
 
