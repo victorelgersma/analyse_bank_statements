03d6fc3c901b950586069a612acc330398163d4b
Victor Elgersma <victor.elgersma41@thg.com>
2024-11-03 09:13:54 +0000

    feat: printing all 3 revolut data frames

--- src/script.py
+++ src/script.py
@@ -1,10 +1,27 @@
 
 import pandas as pd
 
-rev_df = pd.read_csv('../transactions/rev/eur/oct.csv')
+rev_euro_df = pd.read_csv('../data/rev/eur/oct.csv')
+rev_gbp_df = pd.read_csv('../data/rev/gbp/oct.csv')
+rev_usd_df = pd.read_csv('../data/rev/usd/oct.csv')
+
+rev_columns=["Amount", "Description", "Completed Date"]
+
+print("Revolut:\n")
+print("data structure")
+print(rev_euro_df.info()) 
 
 # print(rev_df)
-print('shape: ' + str(rev_df.shape))
 print('info:\n')
-print(rev_df.info())
-print(rev_df.head())
+# check column names
+
+
+
+print("Euro:\n")
+print(rev_euro_df[rev_columns]) 
+
+print("GBP:\n")
+
+print(rev_gbp_df[rev_columns]) 
+print("USD:\n")
+print(rev_usd_df[rev_columns]) 
