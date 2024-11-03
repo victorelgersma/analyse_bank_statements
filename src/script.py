ed5f65b3fd5f00eae24ce00d2c9ca62a756e0570
Victor Elgersma <victor.elgersma41@thg.com>
2024-11-03 09:27:47 +0000

    feat: printing san df

--- src/script.py
+++ src/script.py
@@ -4,11 +4,13 @@ import pandas as pd
 rev_euro_df = pd.read_csv('../data/rev/eur/oct.csv')
 rev_gbp_df = pd.read_csv('../data/rev/gbp/oct.csv')
 rev_usd_df = pd.read_csv('../data/rev/usd/oct.csv')
+san = pd.read_csv('../data/san/oct.csv', sep=';', encoding='ISO-8859-1') # maybe I should add the transactions in savings account
 
 rev_columns=["Amount", "Description", "Completed Date"]
+san_columns=["Debit/Credit", "Date"]
 
-print("Revolut:\n")
-print("data structure")
+print("\nRevolut:\n")
+print("\ndata structure")
 print(rev_euro_df.info()) 
 
 # print(rev_df)
@@ -17,11 +19,16 @@ print('info:\n')
 
 
 
-print("Euro:\n")
+print("\nEuro:\n")
 print(rev_euro_df[rev_columns]) 
 
-print("GBP:\n")
+print("\nGBP:\n")
 
 print(rev_gbp_df[rev_columns]) 
-print("USD:\n")
+print("\nUSD:\n")
 print(rev_usd_df[rev_columns]) 
+
+print("\nSantander \n")
+print(san.info())
+print("\nTransactions\n")
+print(san[san_columns])
